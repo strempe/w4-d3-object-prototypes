50c23e5b2b66a5179b6d490891d454abe328987f
Sara Trempe <88.trempe.sara@gmail.com>
2017-02-27 14:33:33 -0500

    Drawing

--- js/drawing.js
+++ js/drawing.js
@@ -2,6 +2,12 @@ enableDrawing();
 growBoxes();
 
 function enableDrawing() {
+    // holds all of your browser JavaScript code
+    console.dir(window);
+
+    // hold all of your browser HTML code 
+    console.dir(document);
+
     document.addEventListener('mousedown', function(e) {
         var red = Math.round(Math.random()*255);
         var green = Math.round(Math.random()*255);
@@ -12,8 +18,10 @@ function enableDrawing() {
         box.style.left = e.clientX + 'px';
         box.style.width = '10px';
         box.style.height = '10px';
-        box.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue +')';
 
+        // RGB is not JS just CSS - putting it in quotes tells JS that its CSS
+        box.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue +')';
+        box.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue +')';
         document.body.appendChild(box);
     });
 }
